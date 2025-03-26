655a2d37444aa489c9e8d80f9966d413a46e85ea
affuad1903 <affandi.p5@gmail.com>
2025-03-26 12:10:47 +0700

    5th commit finishing final project

--- js/script.js
+++ js/script.js
@@ -1,68 +1,242 @@
-// Javascript External
-// function persegiPanjang(persegiPanjang)
-function persegiPanjang(persegiPanjang){
-    const checkNonActivePP = persegiPanjang.classList;
-    const checkNonActiveP = document.getElementById("buttonPersegi").classList;
-    if(checkNonActivePP.contains("nav-fiture-non-active")==true && checkNonActiveP.contains("nav-fiture-non-active")==true)
-    {
-        checkNonActivePP.replace("nav-fiture-non-active","nav-fiture-active");
-    }else if(checkNonActivePP.contains("nav-fiture-non-active")==true && checkNonActiveP.contains("nav-fiture-non-active")==false){
-        checkNonActiveP.replace("nav-fiture-active","nav-fiture-non-active");
-        checkNonActivePP.replace("nav-fiture-non-active","nav-fiture-active");
-    }
-}
 
-// function persegi(persegi)
-function persegi(persegi){
-    const checkNonActiveP = persegi.classList;
-    const checkNonActivePP = document.getElementById("buttonPersegiPanjang").classList;
-    if(checkNonActiveP.contains("nav-fiture-non-active")==true && checkNonActivePP.contains("nav-fiture-non-active")==true)
-    {
-        checkNonActiveP.replace("nav-fiture-non-active","nav-fiture-active");
-    }else if(checkNonActiveP.contains("nav-fiture-non-active")==true && checkNonActivePP.contains("nav-fiture-non-active")==false){
-        checkNonActivePP.replace("nav-fiture-active","nav-fiture-non-active");
-        checkNonActiveP.replace("nav-fiture-non-active","nav-fiture-active");
+// Deklarasi Untuk Get id button
+const persegi = document.getElementById("buttonPersegi");
+const persegiPanjang = document.getElementById("buttonPersegiPanjang");
+// Deklarasi Untuk Get id display
+const checkPersegiDisplay = document.getElementById('persegiDisplay');
+const checkPersegiPanjangDisplay = document.getElementById('persegiPanjangDisplay');
+
+// function Menampilkan Display Persegi 
+persegi.addEventListener("click", function(){
+
+    // Kondisi untuk mengganti class agar berubah warna buttonnya
+    if(persegi.classList.contains("nav-fiture-non-active") && persegiPanjang.classList.contains("nav-fiture-non-active"))
+        {
+            persegi.classList.replace("nav-fiture-non-active","nav-fiture-active");
+            // untuk memunculkan efek visible dengan menggunakan scrollheight sesuai tinggi element
+            checkPersegiDisplay.style.maxHeight = checkPersegiDisplay.scrollHeight + "px";
+            checkPersegiDisplay.style.opacity="1";
+            checkPersegiDisplay.classList.add('visible');
+            checkPersegiDisplay.classList.remove('unvisible');
+        }else if(persegiPanjang.classList.contains("nav-fiture-non-active")==false && persegi.classList.contains("nav-fiture-non-active")==true && checkPersegiPanjangDisplay.classList.contains('visible')==true){
+            persegiPanjang.classList.replace("nav-fiture-active","nav-fiture-non-active");
+            persegi.classList.replace("nav-fiture-non-active","nav-fiture-active");
+            checkPersegiDisplay.classList.remove('unvisible');
+            checkPersegiDisplay.classList.add('visible');
+            checkPersegiPanjangDisplay.classList.remove('visible');
+            checkPersegiPanjangDisplay.classList.add('unvisible');
+            checkPersegiPanjangDisplay.style.maxHeight ="0px";
+            checkPersegiPanjangDisplay.style.opacity="0";
+            checkPersegiDisplay.style.maxHeight = checkPersegiDisplay.scrollHeight + "px";
+            checkPersegiDisplay.style.opacity="1";
     }
-}
+});
+
+// function  Menampilkan Display Persegi Panjang
+persegiPanjang.addEventListener("click", function(){
+
+    // Kondisi untuk mengganti class agar berubah warna buttonnya
+    if(persegi.classList.contains("nav-fiture-non-active") && persegiPanjang.classList.contains("nav-fiture-non-active"))
+        {
+            persegiPanjang.classList.replace("nav-fiture-non-active","nav-fiture-active");
+            // untuk memunculkan efek visible dengan menggunakan scrollheight sesuai tinggi element
+            checkPersegiPanjangDisplay.style.maxHeight = checkPersegiPanjangDisplay.scrollHeight + "px";
+            checkPersegiPanjangDisplay.style.opacity="1";
+            checkPersegiPanjangDisplay.classList.add('visible');
+            checkPersegiPanjangDisplay.classList.remove('unvisible');
+        }else if(persegiPanjang.classList.contains("nav-fiture-non-active")==true && persegi.classList.contains("nav-fiture-non-active")==false && checkPersegiDisplay.classList.contains('visible')==true){
+            persegi.classList.replace("nav-fiture-active","nav-fiture-non-active");
+            persegiPanjang.classList.replace("nav-fiture-non-active","nav-fiture-active");
+            checkPersegiDisplay.classList.remove('visible');
+            checkPersegiDisplay.classList.add('unvisible');
+            checkPersegiPanjangDisplay.classList.remove('unvisible');
+            checkPersegiPanjangDisplay.classList.add('visible');
+            checkPersegiDisplay.style.maxHeight ="0px";
+            checkPersegiDisplay.style.opacity="0";
+            checkPersegiPanjangDisplay.style.maxHeight = checkPersegiPanjangDisplay.scrollHeight + "px";
+            checkPersegiPanjangDisplay.style.opacity="1";
+    }
+});
+
 
 // function hitung luas persegi
-function hitungLuas(){
+function hitungLuasPersegi(){
     let checkSisi = document.getElementById('sisiLuas');
+    let tampung = document.getElementById("hasilTampungLuas");
+    // memanggil children element
+    childTampung = tampung.children;
     if(checkSisi.value){
         let valueSisi = document.getElementById('sisiLuas').value;
         let hitungLuas = valueSisi*valueSisi;
-        document.getElementById("hasilTampungLuas").innerHTML = 
-            `<p>L = S x S</p>
-             <p>L = ${valueSisi} x ${valueSisi}</p>
-             <p>L = ${hitungLuas}</p>`;
-    }else{
-        document.getElementById("hasilTampungLuas").innerHTML = 
-       ` <p>Anda harus memasukkan nilai</p>`
+        // mengubah teks sesuai dengan input user dan hasil jawaban
+        childTampung[0].innerHTML = "L = S X S";
+        childTampung[1].innerHTML = "L = " + valueSisi + " X " + valueSisi;
+        childTampung[2].innerHTML =  "L = " + hitungLuas;
+        // mengubah opacity dengan mengganti class
+        tampung.classList.add('visible');
+        tampung.classList.remove('unvisible');
+        
+    }else if(checkSisi.value==0 && tampung.classList.contains('unvisible')===true){
+        childTampung[1].innerHTML = "Anda Harus Memasukkan Nilai";
+        childTampung[0].innerHTML= "<br>"
+        childTampung[2].innerHTML= "<br>"
+        tampung.classList.add('visible');
+    }else if(checkSisi.value==0 && tampung.classList.contains('visible')===true){
+        childTampung[1].innerHTML = "Anda Harus Memasukkan Nilai";
+        childTampung[0].innerHTML= "<br>"
+        childTampung[2].innerHTML= "<br>"
     }
 }
-function resetFormLuas(){
+// function reset form luas persegi
+function resetFormLuasPersegi(){
     document.getElementById('sisiLuas').value ="";
-    document.getElementById('hasilTampungLuas').innerHTML ="";
+    let tampung = document.getElementById("hasilTampungLuas");
+    tampung.classList.add('unvisible');
+    tampung.classList.remove('visible');
 }
 
-// functiong hitung keliling persegi
-function hitungKeliling(){
+// function hitung keliling persegi
+function hitungKelilingPersegi(){
     let checkSisi = document.getElementById('sisiKeliling');
+    let tampung = document.getElementById("hasilTampungKeliling");
+    // memanggil children element
+    childTampung = tampung.children;
+
     if(checkSisi.value){
         let valueSisi = document.getElementById('sisiKeliling').value;
-        let hitungKeliling = 4*valueSisi;
-        document.getElementById("hasilTampungKeliling").innerHTML = 
-            `<p>K = 4 x S</p>
-             <p>K = 4 x ${valueSisi}</p>
-             <p>K = ${hitungKeliling}</p>`;
-    }else{
-        document.getElementById("hasilTampungKeliling").innerHTML = 
-       ` <p>Anda harus memasukkan nilai</p>`
+        let hitungKeliling = 4 *valueSisi;
+        // mengubah teks sesuai dengan input user dan hasil jawaban
+        childTampung[0].innerHTML = "K = 4 X S";
+        childTampung[1].innerHTML = "K = 4 X " + valueSisi;
+        childTampung[2].innerHTML =  "K = " + hitungKeliling;
+        // mengubah opacity dengan mengganti class
+        tampung.classList.add('visible');
+        tampung.classList.remove('unvisible');
+    }else if(checkSisi.value==0 && tampung.classList.contains('unvisible')===true){
+        childTampung[1].innerHTML = "Anda Harus Memasukkan Nilai";
+        childTampung[0].innerHTML= "<br>"
+        childTampung[2].innerHTML= "<br>"
+        tampung.classList.add('visible');
+    }else if(checkSisi.value==0 && tampung.classList.contains('visible')===true){
+        childTampung[1].innerHTML = "Anda Harus Memasukkan Nilai";
+        childTampung[0].innerHTML= "<br>"
+        childTampung[2].innerHTML= "<br>"
     }
 }
-function resetFormKeliling(){
+// function form reset keliling persegi
+function resetFormKelilingPersegi(){
     document.getElementById('sisiKeliling').value ="";
-    document.getElementById('hasilTampungKeliling').innerHTML ="";
+    let tampung = document.getElementById("hasilTampungKeliling");
+    tampung.classList.add('unvisible');
+    tampung.classList.remove('visible');
+}
+
+// function hitung luas persegi panjang
+function hitungLuasPP(){
+    let checkPanjang = document.getElementById('panjangPPLuas');
+    let checkLebar = document.getElementById('lebarPPLuas');
+    let tampung = document.getElementById("hasilTampungLuasPP");
+    // memanggil children element
+    childTampung = tampung.children;
+    
+    if(checkPanjang.value && checkLebar.value){
+        let valuePanjang = document.getElementById('panjangPPLuas').value;
+        let valueLebar = document.getElementById('lebarPPLuas').value;
+        if(valuePanjang>valueLebar){
+            let hitungLuas =valuePanjang * valueLebar;
+            // mengubah teks sesuai dengan input user dan hasil jawaban
+            childTampung[0].innerHTML = "L = P X l";
+            childTampung[1].innerHTML = "L = " + valuePanjang + " X " + valueLebar;
+            childTampung[2].innerHTML =  "L = " + hitungLuas;
+            // mengubah opacity dengan mengganti class
+            tampung.classList.add('visible');
+            tampung.classList.remove('unvisible');
+        }else{
+            childTampung[1].innerHTML = "Nilai Lebar Tidak Boleh Lebih Atau Sama Dari Nilai Panjang";
+            childTampung[0].innerHTML= "<br>"
+            childTampung[2].innerHTML= "<br>"
+            tampung.classList.add('visible');
+        } 
+    }else if(checkPanjang.value==0 && checkLebar.value==0 && tampung.classList.contains('unvisible')==true){
+        childTampung[1].innerHTML = "Anda Harus Memasukkan Nilai";
+        childTampung[0].innerHTML= "<br>"
+        childTampung[2].innerHTML= "<br>"
+        tampung.classList.add('visible');
+    }else if(checkPanjang.value==0 && checkLebar.value==0 && tampung.classList.contains('visible')===true){
+        childTampung[1].innerHTML = "Anda Harus Memasukkan Nilai";
+        childTampung[0].innerHTML= "<br>"
+        childTampung[2].innerHTML= "<br>"
+    }else if(checkPanjang.value==0 && checkLebar.value && tampung.classList.contains('visible')===true){
+        childTampung[1].innerHTML = "Anda Harus Memasukkan Nilai Panjang";
+        childTampung[0].innerHTML= "<br>"
+        childTampung[2].innerHTML= "<br>"
+    }else if(checkPanjang.value && checkLebar.value==0 && tampung.classList.contains('visible')===true){
+        childTampung[1].innerHTML = "Anda Harus Memasukkan Nilai Lebar";
+        childTampung[0].innerHTML= "<br>"
+        childTampung[2].innerHTML= "<br>"
+    }
+}
+//function reset form luas persegi panjang
+function resetFormLuasPP(){
+    document.getElementById('panjangPPLuas').value ="";
+    document.getElementById('lebarPPLuas').value ="";
+    let tampung = document.getElementById("hasilTampungLuasPP");
+    tampung.classList.add('unvisible');
+    tampung.classList.remove('visible');
+}
+
+// function hitung keliling persegi panjang
+function hitungKelilingPP(){
+    let checkPanjang = document.getElementById('panjangPPKeliling');
+    let checkLebar = document.getElementById('lebarPPKeliling');
+    let tampung = document.getElementById("hasilTampungKelilingPP");
+    // memanggil children element
+    childTampung = tampung.children;
+    
+    if(checkPanjang.value && checkLebar.value){
+        let valuePanjang = document.getElementById('panjangPPKeliling').value;
+        let valueLebar = document.getElementById('lebarPPKeliling').value;
+        if(valuePanjang>valueLebar){
+            let hitungLuas = 2 * valuePanjang + 2 * valueLebar;
+            // mengubah teks sesuai dengan input user dan hasil jawaban
+            childTampung[0].innerHTML = "L = 2P + 2l";
+            childTampung[1].innerHTML = "L = 2(" + valuePanjang + ") + 2(" + valueLebar + ")";
+            childTampung[2].innerHTML =  "L = " + hitungLuas;
+            // mengubah opacity dengan mengganti class
+            tampung.classList.add('visible');
+            tampung.classList.remove('unvisible');
+        }else{
+            childTampung[1].innerHTML = "Nilai Lebar Tidak Boleh Lebih Atau Sama Dari Nilai Panjang";
+            childTampung[0].innerHTML= "<br>"
+            childTampung[2].innerHTML= "<br>"
+            tampung.classList.add('visible');
+        } 
+    }else if(checkPanjang.value==0 && checkLebar.value==0 && tampung.classList.contains('unvisible')==true){
+        childTampung[1].innerHTML = "Anda Harus Memasukkan Nilai";
+        childTampung[0].innerHTML= "<br>"
+        childTampung[2].innerHTML= "<br>"
+        tampung.classList.add('visible');
+    }else if(checkPanjang.value==0 && checkLebar.value==0 && tampung.classList.contains('visible')===true){
+        childTampung[1].innerHTML = "Anda Harus Memasukkan Nilai";
+        childTampung[0].innerHTML= "<br>"
+        childTampung[2].innerHTML= "<br>"
+    }else if(checkPanjang.value==0 && checkLebar.value && tampung.classList.contains('visible')===true){
+        childTampung[1].innerHTML = "Anda Harus Memasukkan Nilai Panjang";
+        childTampung[0].innerHTML= "<br>"
+        childTampung[2].innerHTML= "<br>"
+    }else if(checkPanjang.value && checkLebar.value==0 && tampung.classList.contains('visible')===true){
+        childTampung[1].innerHTML = "Anda Harus Memasukkan Nilai Lebar";
+        childTampung[0].innerHTML= "<br>"
+        childTampung[2].innerHTML= "<br>"
+    }
+}
+// function reset form keliling persegi panjang
+function resetFormKelilingPP(){
+    document.getElementById('panjangPPKeliling').value ="";
+    document.getElementById('lebarPPKeliling').value ="";
+    let tampung = document.getElementById("hasilTampungKelilingPP");
+    tampung.classList.add('unvisible');
+    tampung.classList.remove('visible');
 }
 
 // Tahun akan berganti terus
